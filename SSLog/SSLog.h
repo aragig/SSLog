07de6a5ac42ae297019745ac50160dd6d78e1982
Toshihiko Arai <araemonz@gmail.com>
2024-09-16 11:55:56 +0900

    Initial Commit

---
+++ SSLog/SSLog.h
@@ -0,0 +1,18 @@
+//
+//  SSLog.h
+//  SSLog
+//
+//  Created by Toshihiko Arai on 2024/09/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SSLog.
+FOUNDATION_EXPORT double SSLogVersionNumber;
+
+//! Project version string for SSLog.
+FOUNDATION_EXPORT const unsigned char SSLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSLog/PublicHeader.h>
+
+
